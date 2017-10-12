0d344ffb9fc539191599cae67b54a2de8e732cdf
belgeu <belgeu777@gmail.com>
2017-10-12 19:22:51 +0300

    Square 2.0 released! Added option parser (check: python square.py -h). Fixed bugs from v1.1.

--- square.py
+++ square.py
@@ -1,20 +1,53 @@
-import sys
+#!/usr/bin/python
+
+import argparse
 import math
+import sys
 
-# ax^2 + bx + c = 0
+v = 'Square 2.0'
+parser = argparse.ArgumentParser(description='Script to solve quadratic equation.\nax2 + bx + c = 0')
+parser.add_argument("a", type=float,
+                    help="Quadratic coefficient 'a'. Non-zero value.")
+parser.add_argument("b", type=float,
+                    help="Linear coefficient 'b'")
+parser.add_argument("c", type=float,
+                    help="Coefficient 'c'")
+parser.add_argument('-v', '--version', action='version',
+                    version=v, help="Show program's version number and exit.")
+args = parser.parse_args()
 
-a = float(sys.argv[1])
-b = float(sys.argv[2])
-c = float(sys.argv[3])
 
-D = math.pow(b,2) - 4 * a * c
-print "D = %s" % D
-if D > 0:
-	x_one = (-b + math.sqrt(D)) / (2*a)
-	x_two = (-b - math.sqrt(D)) / (2*a)
-	print "x1 = %s, x2 = %s" % (x_one, x_two)
-elif D == 0:
-	x = -b/(2*a)
-	print "x = %s" % x
-else:
-	print 'No value'
+def discriminant(i, j, k):
+    disc = math.pow(j, 2) - 4 * i * k
+    return disc
+
+
+def is_zero(z):
+    if z == 0:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    a = args.a
+    b = args.b
+    c = args.c
+
+    if is_zero(a):
+        sys.exit("First argument should not equal '0'")
+
+    D = discriminant(a, b, c)
+
+    print "D = %s" % D
+
+    if D > 0:
+        x_one = (-b + math.sqrt(D)) / (2*a)
+        x_two = (-b - math.sqrt(D)) / (2*a)
+        print "x1 = %s, x2 = %s" % (round(x_one, 2), round(x_two, 2))
+    elif D == 0:
+        x = -b/(2*a)
+        if x == 0:
+            x = abs(x)
+        print "x = %s" % round(x, 2)
+    else:
+        print 'No value'
