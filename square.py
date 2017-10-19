6e4658f3e9818c45f031e7a480dedcfb0f9c0b60
katejanis <janisjamaika@gmail.com>
2017-10-19 22:31:06 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,31 +1,55 @@
-#v1.2
+#!/usr/bin/python
+# coding=utf-8
 
-import sys
+import argparse
 import math
+import sys
 
-# ax^2 + bx + c = 0
+v = 'Square 2.0'
+parser = argparse.ArgumentParser(description='Script to solve quadratic equation.\n[a]⋅x² + [b]⋅x + [c] = 0')
+parser.add_argument('a', type=float,
+                    help='Quadratic coefficient \'a\'. Non-zero value.')
+parser.add_argument('b', type=float,
+                    help='Linear coefficient \'b\'')
+parser.add_argument('c', type=float,
+                    help='Coefficient \'c\'')
+parser.add_argument('-v', '--version', action='version',
+                    version=v, help='Show program\'s version number and exit.')
+args = parser.parse_args()
 
-a = float(sys.argv[1])
-b = float(sys.argv[2])
-c = float(sys.argv[3])
 
-def discriminant(x, y , z):
-    d = math.pow(y,2) - 4 * x * z
-    return d
-
-D = discriminant(a, b, c)
+def discriminant(i, j, k):
+    disc = math.pow(j, 2) - 4 * i * k
+    return disc
 
 
-if a == 0:
-    print 'First argument should not be equal zero'
-else:
-    print "D = %s" % D
+def is_zero(z):
+    if z == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    a = args.a
+    b = args.b
+    c = args.c
+
+    if is_zero(a):
+        sys.exit('First argument should not equal \'0\'')
+
+    D = discriminant(a, b, c)
+
+    print 'D = {}'.format(round(D, 2))
+
     if D > 0:
         x_one = (-b + math.sqrt(D)) / (2*a)
         x_two = (-b - math.sqrt(D)) / (2*a)
-        print "x1 = %s, x2 = %s" % (x_one, x_two)
+        print 'x1 = {}; x2 = {}'.format(round(x_one, 2), round(x_two, 2))
     elif D == 0:
         x = -b/(2*a)
-        print "x = %s" % x
+        if x == 0:
+            x = abs(x)
+        print "x = {}".format(round(x, 2))
     else:
         print 'No value'
