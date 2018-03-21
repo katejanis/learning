6f83c2cf1a643c6e93a5ab92fed9910dc0de04b9
Kate Janis <janisjamaika@gmail.com>
2018-03-21 21:22:58 +0200

    Extend autotests

--- banking/autocheck.py
+++ banking/autocheck.py
@@ -3,7 +3,18 @@ import pexpect
 
 class BankingAutocheck(unittest.TestCase):
     timeout = 3
-    def test_age_less_than_12(self):
+   
+     def test_age_11(self):
+        bank = pexpect.spawn('ruby run.rb')
+        bank.expect(['Banking v0.3', 'Customer age (integer):'], self.timeout)
+        bank.sendline('11')
+        bank.expect('Customer service: false', self.timeout)
+        bank.expect('Offer membership: false', self.timeout)
+        bank.expect('Discount rate: false', self.timeout)
+        bank.interact()
+        bank.close()
+
+     def test_age_less_than_12(self):
         bank = pexpect.spawn('ruby run.rb')
         bank.expect(['Banking v0.3', 'Customer age (integer):'], self.timeout)
         bank.sendline('12')
